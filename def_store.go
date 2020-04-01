c23f0ccfa3ff64b886e003db305094dc34412e78
Tudor Hulban <prindex2010@gmail.com>
2020-04-01 20:35:31 +0300

    Update def_store.go

--- def_store.go
+++ def_store.go
@@ -1,5 +1,12 @@
 package badgerwrap
 
+// KV is key value for the NoSQL DB.
+type KV struct {
+	key   []byte
+	value []byte
+}
+
+// should be exported
 type store interface {
 	SetKV(pKV KV) error
 	GetV(pKey []byte) ([]byte, error)
