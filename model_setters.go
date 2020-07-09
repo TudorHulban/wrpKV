3ec7f1ed0b7546d3e067dea947315b5a0c9c4f08
Tudor Hulban <prindex2010@gmail.com>
2020-07-09 11:39:13 +0300

    Update model_setters.go

--- model_setters.go
+++ model_setters.go
@@ -7,16 +7,16 @@ import (
 )
 
 // SetKV sets or updates key in store.
-func (s bstore) Set(pKV KV) error {
+func (s bstore) Set(theKV KV) error {
 	return s.b.Update(func(txn *badger.Txn) error {
-		return txn.Set([]byte(pKV.key), []byte(pKV.value))
+		return txn.Set([]byte(theKV.key), []byte(theKV.value))
 	})
 }
 
 // setKVTTL can be used for inserts and updates. Time To Live in seconds.
-func (s bstore) SetTTL(pKV KV, pTTLSecs int) error {
+func (s bstore) SetTTL(theKV KV, ttlSecs int) error {
 	return s.b.Update(func(txn *badger.Txn) error {
-		entry := badger.NewEntry([]byte(pKV.key), []byte(pKV.value)).WithTTL(time.Second * time.Duration(pTTLSecs))
+		entry := badger.NewEntry([]byte(theKV.key), []byte(theKV.value)).WithTTL(time.Second * time.Duration(ttlSecs))
 		errSet := txn.SetEntry(entry)
 		return errSet
 	})
