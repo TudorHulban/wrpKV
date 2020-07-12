5cdff6e0e91e2f55d1db56dfdc96ecfde2475893
Tudor Hulban <tudi@J3710>
2020-07-12 15:23:29 +0300

    added kv deletion

--- interface.go
+++ interface.go
@@ -16,12 +16,14 @@ type Store interface {
 	Set(KV) error
 	// Inserts or updates KV in store. Time To Live in seconds.
 	SetTTL(KV, int) error
-	// Returns value of key if found. If not found it returns empty slice
+	// Returns value for passed key if found. If not found it returns empty slice
 	// and an error not nil.
 	GetVByK([]byte) ([]byte, error)
 	// Returns a slice of KV if prefix found.
 	// If not found it returns empty slice.
 	GetKVByPrefix([]byte) ([]KV, error)
+	// Deletes KV bassed on key.
+	DeleteKVByK([]byte) error
 	// Close closes the opened KV store.
 	Close() error
 }
